dc1460ab71dfdaec1ec0d58261dbd0fd4a46f01f
Xevi <xevi@MacBook-MacBook-Pro-de-Xevi.local>
2013-07-02 18:04:23 +0200

    First commit

---
+++ prova/src/main/java/com/example/prova/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.prova;
+
+import android.net.Uri;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.bt);
+        bt.setOnClickListener(new OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent browserIntent =
+                        new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://www.salicru.com"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+}
+    
+
